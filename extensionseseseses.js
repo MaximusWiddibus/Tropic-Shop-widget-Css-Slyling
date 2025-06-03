745277f7c2897969dccf1ec5c59f49a9bd53630b
MaximusWiddibus <maxim@swiftyai.de>
2025-06-03 12:22:49 +0200

    Update and rename extensionsesesese.js to extensionseseseses.js

--- extensionseseseses.js
+++ extensionseseseses.js
@@ -41,7 +41,7 @@ const genPDF = {
 const InfoText = {
   name: 'InfoText',
   type: 'response',
-  match: ({ trace } = {}) => trace?.type === 'InfoText' || trace?.payload === 'InfoText',
+  match: ({ trace } = {}) => trace?.type === 'InfoText' || trace?.payload?.name === 'InfoText',
   render: ({ element }) => {
     const target = document.querySelector('.jsli361.jsli363');
     
